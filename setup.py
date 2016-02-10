a7df7c9abb57adbf2db569956972e56afdc478dd
ipetrash <gil9red@gmail.com>
2016-02-10 10:28:02 +0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# A very simple setup script to create a single executable
+#
+# Run the build process by running the command 'python setup.py build'
+#
+# If everything works well you should find a subdirectory in the build
+# subdirectory that contains the files needed to run the script without Python
+
+from cx_Freeze import setup, Executable
+
+executables = [
+    Executable('main.py')
+]
+
+setup(name='wallpaper_from_himawari',
+      version='0.1',
+      description='wallpaper_from_himawari',
+      executables=executables
+      )
